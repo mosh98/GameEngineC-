bff2a1d0ba2dcdc258cfc966f5efc775cfbc503c
moslehmahamud <mosleh.edu@gmail.com>
2020-06-19 11:28:40 +0200

    Initial Commit

---
+++ GameEngineInC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngineInC
+//
+//  Created by Mosleh Mahamud on 2020-06-19.
+//  Copyright © 2020 Mosleh Mahamud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
